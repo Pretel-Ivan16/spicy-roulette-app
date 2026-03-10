87c9e019b8a7481f7475551f93efa21d4b4bbd2b
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-10 01:26:06 -0300

    refactor game store to implement context API for state management

--- spicy-roulette-app/src/store/game/gameStore.tsx
+++ spicy-roulette-app/src/store/game/gameStore.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { createContext, useState, type ReactNode } from 'react'
+import type { Player } from '../../types/player'
+import type { Category } from '../../types/category'
 
-function gameStore() {
-  return (
-    <div>
-      
-    </div>
-  )
+interface GameContextType {
+  players: Player[];
+  setPlayers: (players: Player[]) => void;
+  playerCount: number;
+  setPlayerCount: (count: number) => void;
+  selectedCategory: Category | null;
+  setSelectedCategory: (category: Category) => void;
 }
 
-export default gameStore
+export const GameContext = createContext<GameContextType | undefined>(undefined);
+
+export function GameProvider({children}: {children: ReactNode }){
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [playerCount, setPlayerCount] = useState<number>(0);
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+  
+  return (
+    <GameContext.Provider value={{players, setPlayers, playerCount, setPlayerCount, selectedCategory, setSelectedCategory}}>
+      {children}
+    </GameContext.Provider>
+  );
+}
+
