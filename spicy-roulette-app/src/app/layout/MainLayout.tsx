134cf5d05803a9dd06b61004d5b1a4f638caa7ff
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-10 01:25:59 -0300

    refactor layout components for improved structure and styling

--- spicy-roulette-app/src/app/layout/MainLayout.tsx
+++ spicy-roulette-app/src/app/layout/MainLayout.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
 
 function MainLayout() {
   return (
-    <div>
-      
+    <div className='min-h-screen bg-linear-to-br from-[#030712] via-[#0f172a] to-[#030712]'>
+      <header className='bg-linear-to-r from-[#0f172a]/80 to-[#1e293b]/80 backdrop-blur-md'>
+        <Header variant='main'/>
+      </header>
+      <main className='flex flex-col items-center justify-center px-4 py-12 bg-linear-to-b from-[#10b981]/5 via-transparent to-transparent'>
+        <Outlet/>
+      </main>
     </div>
   )
 }
